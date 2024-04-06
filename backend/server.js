6bb113ed7d8df05776b43a61836fb72af72e3d6e
Tenzin Tsundue <tentsundue28@gmail.com>
2024-04-05 17:16:55 -0700

    added middleware handling and environment variables.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
+app.use(express.json()); // if anty requests come in, parse json data from request body and attatch to request object
+app.use((req, res, next) => {
+    console.log(`${req.path}, ${req.method}`); // log the path and method of each request
+    next(); // move on to the next middleware
+})
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send("this is the home page");
     console.log("home page");
 })
 
-app.listen(3000);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
